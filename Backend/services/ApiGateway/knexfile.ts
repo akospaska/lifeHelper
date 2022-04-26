c838bce30b2a1ef802da889aaf5698da5d5351bf
akospaska <akos.paska9011@gmail.com>
2022-04-26 13:22:02 +0200

    base gateway done

--- Backend/services/ApiGateway/knexfile.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path'
-const envFilePath = path.resolve(__dirname, '../env_variables/dev/auth/web/.env.auth.web')
-require('dotenv').config({ path: envFilePath })
-
-import { validatedSqlConnectionVariables } from './web/validation/server'
-
-module.exports = validatedSqlConnectionVariables
