a4b91960d1b2f76064bbe0fe73d8a394ab8b1e93
Ákos <akos.paska@risingstack.com>
2022-05-06 11:50:02 +0200

    forgotpasswordrequest route connected

---
+++ Backend/services/ApiGateway/routes/api/auth/forgotPasswordRequest/index.ts
@@ -0,0 +1,27 @@
+import { ResponseToolkit, Request } from 'hapi'
+
+import { AxiosResponse } from 'axios'
+
+import { authServiceApi } from '../../../../api/services/authService'
+
+export const forgotPasswordRequestRoute = {
+  method: 'POST',
+  path: '/api/auth/forgotpasswordrequest',
+  handler: async (req: Request, h: ResponseToolkit, err?: Error) => {
+    //const x = await authorizateUserRequest(req)
+
+    //1. send threqe loginDetails to the auth service
+
+    const validateLoginAxiosResponse: AxiosResponse = await authServiceApi.post(
+      '/api/forgotpasswordrequest',
+      req.payload
+    )
+
+    const registerResult = validateLoginAxiosResponse.data
+    //2. set the cookie of the header
+
+    const response = h.response(registerResult).code(200)
+
+    return response
+  },
+}
