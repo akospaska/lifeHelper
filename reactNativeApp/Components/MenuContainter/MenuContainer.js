f508959339cff29434bb6668cb63b1248a237595
askopaska <akos.paska9011@gmail.com>
2022-06-18 20:57:45 +0200

    grocery list scrolls done

--- reactNativeApp/Components/MenuContainter/MenuContainer.js
+++ reactNativeApp/Components/MenuContainter/MenuContainer.js
@@ -1,27 +1,39 @@
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
-import React from "react";
-import { View } from "native-base";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import React from 'react'
+import { View } from 'native-base'
 
-import CreateNewItem from "./CreateNewItem/CreateNewItem";
-import CreateNewCategory from "./CreateNewCategory/CreateNewCategory";
+import CreateNewItem from './CreateNewItem/CreateNewItem'
+import CreateNewCategory from './CreateNewCategory/CreateNewCategory'
 
 export const Example = (props) => {
   return (
     <View
       style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        backgroundColor: "#292524",
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        backgroundColor: '#292524',
         paddingBottom: 20,
       }}
     >
-      <CreateNewItem fake={props.fake} forceRefresh={props.forceRefresh} selectedGroceryGroupId={props.selectedGroceryGroupId} />
+      <CreateNewItem
+        fake={props.fake}
+        forceRefresh={props.forceRefresh}
+        selectedGroceryGroupId={props.selectedGroceryGroupId}
+        setOpenedCategoryId={props.setOpenedCategoryId}
+      />
       <CreateNewCategory forceRefresh={props.forceRefresh} selectedGroceryGroupId={props.selectedGroceryGroupId} />
     </View>
-  );
-};
+  )
+}
 
 export default (props) => {
-  return <Example fake={props.fake} forceRefresh={props.forceRefresh} selectedGroceryGroupId={props.selectedGroceryGroupId} />;
-};
+  return (
+    <Example
+      fake={props.fake}
+      forceRefresh={props.forceRefresh}
+      selectedGroceryGroupId={props.selectedGroceryGroupId}
+      setOpenedCategoryId={props.setOpenedCategoryId}
+    />
+  )
+}
