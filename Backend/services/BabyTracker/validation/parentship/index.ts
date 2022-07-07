ac3299832b3ab1bcc2fbb6d123191f7fe1d8fff6
askopaska <akos.paska9011@gmail.com>
2022-07-07 08:43:50 +0200

    BE_babytracker_acceptinvitation_parent_in_progress

--- Backend/services/BabyTracker/validation/parentship/index.ts
+++ Backend/services/BabyTracker/validation/parentship/index.ts
@@ -20,3 +20,25 @@ export interface getCheckParentShipStatusRequestBodyType {
   accountId: number
 }
 //------ ------------ ------//
+
+//-------checkParentShipStatus  ------//
+const getAcceptParentInvitationRequestBodySchema = Joi.object().keys({
+  accountId: Joi.number().positive().integer().required(),
+  invitationId: Joi.number().positive().integer().required(),
+})
+
+export const getValidatedAcceptInvitationRequestBody = (requestBody: acceptInvitationRequestBody) => {
+  const validatedRequestBody: acceptInvitationRequestBody = Joi.attempt(
+    requestBody,
+    getAcceptParentInvitationRequestBodySchema,
+    globalJoiOptions
+  )
+
+  return validatedRequestBody
+}
+
+export interface acceptInvitationRequestBody {
+  accountId: number
+  invitationId: number
+}
+//------ ------------ ------//
