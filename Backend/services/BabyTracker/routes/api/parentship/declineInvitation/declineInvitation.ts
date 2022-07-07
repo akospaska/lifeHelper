851d2373ccd3ae08ef856eb34b66c1ae7dc0138f
askopaska <akos.paska9011@gmail.com>
2022-07-07 19:26:48 +0200

    BE_babytracker_declineinvitation route added

--- Backend/services/BabyTracker/routes/api/parentship/declineInvitation/declineInvitation.ts
+++ Backend/services/BabyTracker/routes/api/parentship/declineInvitation/declineInvitation.ts
@@ -1,19 +1,25 @@
 import { ResponseToolkit, Request } from 'hapi'
+import { declineInvitation, isTheInvitationBelongsToTheAccountId } from '../../../../dataAccessLayer/parentship'
 
-import {
-  getCheckParentShipStatusRequestBodyType,
-  getValidatedCheckParentShipRequestBody,
-} from '../../../../validation/parentship'
+import { acceptInvitationRequestBody, getValidatedAcceptInvitationRequestBody } from '../../../../validation/parentship'
 
 export const declineInvitationRoute = {
   method: 'post',
   path: '/api/parentship/declineinvitation',
   handler: async (req: Request, h: ResponseToolkit, err?: Error) => {
     //1. validate is accountId exists in the request body
-    const requestBody = req.payload as unknown as getCheckParentShipStatusRequestBodyType
+    const requestBody = req.payload as unknown as acceptInvitationRequestBody
 
-    const { accountId } = getValidatedCheckParentShipRequestBody(requestBody)
+    const { accountId, invitationId } = getValidatedAcceptInvitationRequestBody(requestBody)
 
+    //1. check is the accountId is invited  by the invitationid?
+
+    await isTheInvitationBelongsToTheAccountId(accountId, invitationId)
+
+    //2. delete the invitation
+    await declineInvitation(invitationId)
+
+    //3. return the response body
     const response = h.response({ isValid: true }).code(200)
 
     return response
