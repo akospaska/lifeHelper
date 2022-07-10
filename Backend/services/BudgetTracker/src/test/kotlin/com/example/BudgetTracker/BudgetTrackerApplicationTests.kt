d90895106780f79f4bd8955f8970b7c9a4559466
akospaska <akos.paska9011@gmail.com>
2022-07-10 15:45:17 +0100

    kotlin inited

---
+++ Backend/services/BudgetTracker/src/test/kotlin/com/example/BudgetTracker/BudgetTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.BudgetTracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BudgetTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
